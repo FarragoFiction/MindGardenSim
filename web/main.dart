298332adac193878fe0bd8f4f34f0f598804ec77
jadedResearcher <gtg287y@gmail.com>
2020-03-03 21:15:55 -0500

    no gamer tags allowed for names, sorry

--- web/main.dart
+++ web/main.dart
@@ -7,9 +7,15 @@ import 'scripts/Weed.dart';
 
 void main() async {
   Game game = new Game();
-
+  window.addEventListener("keydown", plzStopSpace );
   game.display(querySelector('#output'));
   await Weed.slurpPhrases();
   PhraseSettingsBox.display(querySelector('#output'));
   //Phrase.nextPhrase().display(querySelector('#output'));
+}
+
+void plzStopSpace(Event e) {
+  if(e.target == querySelector("body")) {
+    e.preventDefault();
+  }
 }
